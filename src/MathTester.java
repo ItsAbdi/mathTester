d82ba48622678ac3846210a43c476861b6549295
Abdi Sirat <abdikadar.sirat.2021@student.lhschools.org>
2018-09-13 14:12:21 -0400

    Created the mathTester class and performed math

--- src/MathTester.java
+++ src/MathTester.java
@@ -1,11 +1,36 @@
 public class MathTester {
 
-    public static void main(String[]  args){}
-    String firstNumber;
-    String secondNumber;
+    public static void main(String[] args){
+        String firstWord;
+        String secondWord;
+        int firstNumber,secondNumber, total;
 
-    firstNumber="10";
-    secondNumber= "5";
 
-    System.out.println(firstnumber+secondNumber);
+
+
+        firstWord= "dog";
+        secondWord="house";
+        firstNumber=5;
+        secondNumber=10;
+        total= firstNumber+secondNumber;
+
+        System.out.println(total);
+        total=total+10;
+        System.out.println(total);
+
+
+
+        /*System.out.println(firstWord+secondWord);
+        System.out.println(firstWord+(firstNumber+secondNumber));
+        System.out.println("firstNumber+secondNumber is "+ (firstNumber+secondNumber));
+        System.out.println("firstNumber-secondNumber is "+ (firstNumber-secondNumber));
+        System.out.println("firstNumber*secondNumber is "+ (firstNumber*secondNumber));
+        System.out.println("secondNumber/firstNumber is "+ (secondNumber/firstNumber));
+        System.out.println("The modules of the secondNumber by the firstNumber " +
+                "is "+ (secondNumber%firstNumber));*/
+
+
+    }
+
+
 }
