c8138c301f6e4c5247a1a2a35762b98ad649e2f9
Abdi Sirat <abdikadar.sirat.2021@student.lhschools.org>
2018-09-13 13:09:07 -0400

    Created an intellija project

---
+++ src/MathTester.java
@@ -0,0 +1,11 @@
+public class MathTester {
+
+    public static void main(String[]  args){}
+    String firstNumber;
+    String secondNumber;
+
+    firstNumber="10";
+    secondNumber= "5";
+
+    System.out.println(firstnumber+secondNumber);
+}
